6c89f0e3c04209a44e2b56561c2c66d6d921c55f
James Cole <james@openstack.org>
2020-06-06 11:42:58 -0500

    Setup initial code

---
+++ main.js
@@ -0,0 +1,39 @@
+const menu = {
+    _courses: {
+        appetizers: [],
+        mains: [],
+        desserts: []
+    },
+    get appetizers() {
+
+    },
+    set appetizers() {
+
+    },
+    get mains() {
+
+    },
+    set mains() {
+
+    },
+    get desserts() {
+
+    },
+    set desserts() {
+
+    },
+    get courses() {
+        return {
+            appetizers: this.appetizers,
+            mains: this.mains,
+            desserts: this.desserts
+        }
+    }
+    addDishToCourse(courseName, dishName, dishPrice) {
+        return {
+            const dish = {
+                
+            }
+        }
+    }
+};
